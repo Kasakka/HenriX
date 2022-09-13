6d5fd21a8434357b11b38344c733699b92b9268d
Kasakka <henri.matero@gmail.com>
2022-09-14 00:12:43 +0300

    Fonts, basic layout etc...

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
 use sycamore::prelude::*;
 
+#[component]
+fn NavBar<G: Html>(cx: Scope) -> View<G> {
+    view! { cx,
+        header(class="topnav") {
+            a(href="#", class="topnav--logo") {"HenriX"}
+            a(href="#") {"Link"}
+            a(href="#") {"Link"}
+            a(href="#footer") {"Footer"}
+        }
+    }
+}
+
+#[component]
+fn Content<G: Html>(cx: Scope) -> View<G> {
+    view! { cx,
+        section(class="content"){
+            div(class="flex"){
+
+            }
+        }
+    }
+}
+
+
+#[component]
+fn Footer<G: Html>(cx: Scope) -> View<G> {
+    view! { cx,
+        footer(id="footer", class="footer") {
+            a(href="#") {"Footiboogi"}
+        }
+    }
+}
+
+
 fn main() {
     sycamore::render(|cx| view! { cx,
-        div {
-            div(class="flex"){
-                p { a(href="#test"){"Hello, World!"} }
-                p { "Hello, World!" }
-                p { "Hello, World!" }
-                p { "Hello, World!" }
-            }
-            div(id="test") {
-                p {"Tadaa"}
-            }
+        main {
+            NavBar { }
+            Content { }
+            Footer { }
         }
     });
 }
