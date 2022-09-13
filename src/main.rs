153928073e619f3f713ab8e1b7d2e1cbc28f4215
Kasakka <henri.matero@gmail.com>
2022-09-14 01:34:16 +0300

    Building continues

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,29 @@ fn NavBar<G: Html>(cx: Scope) -> View<G> {
 fn Content<G: Html>(cx: Scope) -> View<G> {
     view! { cx,
         section(class="content"){
-            div(class="flex"){
-
+            div(class="intro"){
+                h1(class="intro--title") {"HENRI MATERO"}
+                p(class="intro--lead") {"Tähän joku pikku insert meikästä ja ehkä pinkkejä keywordejä?"}
+            }
+            section(class="cards"){
+                div(class="flex cards--container"){
+                    a(class="card") {
+                        img(class="card--image", src="assets/images/roses.jpg")
+                        h3(class="card--title"){"Jotain"}
+                    }
+                    a(class="card") {
+                        img(class="card--image", src="assets/images/koodari.jpg")
+                        h3(class="card--title"){"CV"}
+                    }
+                    a(class="card") {
+                        img(class="card--image", src="assets/images/yeller.jpg")
+                        h3(class="card--title"){"Bio"}
+                    }
+                }
             }
         }
     }
 }
-
 
 #[component]
 fn Footer<G: Html>(cx: Scope) -> View<G> {
