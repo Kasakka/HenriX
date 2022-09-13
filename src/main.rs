9eaa25af9ebb34d5b5d6580833008ead818a6c9f
Kasakka <henri.matero@gmail.com>
2022-09-13 21:14:21 +0300

    Säätöä

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use sycamore::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    sycamore::render(|cx| view! { cx,
+        div {
+            div(class="flex"){
+                p { "Hello, World!" }
+                p { "Hello, World!" }
+                p { "Hello, World!" }
+                p { "Hello, World!" }
+            }
+        }
+    });
 }
